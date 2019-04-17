69bc07a21698c2105ed3c5f610f9581ca86610d5
rush <rushastronomo0415@gmail.com>
2019-04-17 10:12:42 -0700

    adds initial project files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code here
+var http = require("http");
+
+http.createServer (function (request, response) {
+    response.writeHead(200, {"content-type":"text/plain"});
+    response.end("Hello World");
+})
+.listen(8080, "127.0.0.1");
+console.log("Server running at http://127.0.0.1:8080/");
